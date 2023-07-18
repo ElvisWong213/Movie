6465f82ae26eed85f85f2ce4ac681c657ef9f02e
Elvis Wong <40566101+ElvisWong213@users.noreply.github.com>
2023-07-18 01:59:38 +0100

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Elvis on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
